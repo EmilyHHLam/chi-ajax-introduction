d79a6545dd67938d17787641c23fa4dd0f9e28f4
Christopher Black <chris@primeacademy.io>
2017-03-15 14:04:06 -0500

    AJAX - GET and POST requests.

--- server/app.js
+++ server/app.js
@@ -4,10 +4,28 @@ var express = require('express');
 var app = express();
 var port = 5000; // 5000 used for localhost
 
+var products = [{make: "Current Designs", model:"Tsunami"},
+                {make:"Geo", model:"Tracker"}];
+
 // By default this looks for index.html
 app.use(express.static('server/public', {
   index: 'views/index.html'
 }));
+
+app.get("/products", function(req, res){
+  res.send(products);
+});
+
+// npm install body-parser --save
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/newProduct", function(req, res){
+  var newProduct = req.body;
+  products.push(newProduct);
+  console.log(newProduct);
+  res.sendStatus(200);
+});
 
 app.listen(port);
 console.log("Listening on port ", port);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,45 @@
-var products = [];
-
 $(document).ready(function(){
   console.log('jQuery sourced.');
+
+  refreshProducts();
 
   $('form').on('submit', function(event) {
     event.preventDefault();
     var product = {};
     product.make = $("#make").val();
     product.model = $("#model").val();
-    console.log(product);
+
+    $.ajax({
+      type: 'POST',
+      url: '/newProduct',
+      data: product,
+      success: function(response) {
+        console.log("SUCCESS!!!");
+        refreshProducts();
+      }
+    });
+    //products.push(product);
   });
 });
+
+function refreshProducts() {
+  $.ajax({
+    type: 'GET',
+    url: '/products',
+    success: function(response) {
+      console.log(response);
+      displayProducts(response);
+    }
+  });
+}
+
+function displayProducts(products) {
+  $("#productsTable").empty();
+  for(var i = 0; i < products.length; i++) {
+    $("#productsTable").append("<span>" +
+                               products[i].make +
+                               ": " +
+                               products[i].model +
+                               "</span><br>");
+  }
+}
